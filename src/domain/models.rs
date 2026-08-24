5fbd6096772e70b338cc8ac0ee2114fb35a98129
bartiam <bartiamwork@yandex.com>
2026-08-24 18:15:44 +0500

    Added new structures to models.rs for creating chats.

--- src/domain/models.rs
+++ src/domain/models.rs
@@ -21,3 +21,29 @@ pub struct User {
     #[serde(skip_serializing_if = "Option::is_none")]
     pub bio: Option<String>,
 }
+
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
+#[serde(rename_all = "snake_case")]
+pub enum ChatType {
+    Private,
+    Group,
+}
+
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
+#[serde(rename_all = "snake_case")]
+pub enum MemberRole {
+    Admin,
+    Member,
+}
+
+pub struct Chat {
+    pub id: Uuid,
+    pub chat_type: ChatType,
+    pub title: Option<String>
+}
+
+pub struct ChatMember {
+    pub chat_id: Uuid,
+    pub user_id: Uuid,
+    pub role: MemberRole,
+}
